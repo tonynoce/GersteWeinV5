d7e9e2dd8c16e6b15581f41f087fa1b192ba5da1
tonynoce <luke.1917@gmail.com>
2022-12-15 00:26:00 -0300

    modified:   packages/svelte-kit/src/lib/components/TxHashModal.svelte

--- packages/svelte-kit/src/lib/components/TxHashModal.svelte
+++ packages/svelte-kit/src/lib/components/TxHashModal.svelte
@@ -46,24 +46,24 @@
 
 <style>
     .wrapper{
+        background-color: rgb(10, 48, 73);
+        background-image: radial-gradient(farthest-corner at 20px 20px,rgba(31, 127, 172, 0.7) 25%, rgba(135, 20, 189, 0.25) 80%);
         position: fixed;
         top: 25vh;
-        width: 35vw;
-        height: 25vh;
-        overflow: auto;
-
-        box-shadow: 0px 0px 120px 10px rgba(255, 46, 238, 0.75);
+        width: 45vw;
+        min-height: 15vh;
+        border-radius: 5px;
+        box-shadow: 0px 0px 120px 10px rgba(2, 115, 207, 0.75);
     }
-
+    
     .modal {
-        background-color: rgb(18, 65, 97);
         max-width: 80vw;
         padding: 1rem;
         margin: auto;
-
     }
 
     .buttonWrapper {
+        margin-top: 20%;
         text-align: right;
     }
 
